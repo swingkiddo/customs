63bf17669c8359fdec48d57f742eded3a1ff417b
swingkiddo <yoshibeatsu@gmail.com>
2023-05-25 13:50:16 +0300

    init

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,17 @@
+package apiserver
+
+// APIServer ...
+type APIServer struct {
+	config *Config
+}
+
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+	}
+}
+
+// Start ...
+func (s *APIServer) Start() error {
+	return nil
+}
